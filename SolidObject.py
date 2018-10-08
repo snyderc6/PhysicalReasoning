b6ebc997402286591cf0054a94cefcb2f83abe49
joelmichelson <joelmichelson@gmail.com>
2018-10-08 13:58:35 -0500

    moved code from notebook to findObjects

--- SolidObject.py
+++ SolidObject.py
@@ -8,5 +8,6 @@ class SolidObject():
         allpixels = np.transpose(array.nonzero())
         self.center = tuple(np.floor(np.mean(allpixels,axis=0)))
         self.ropeIds = []
+        self.area = sum(sum(array))
         self.ropeAttachPoints = []
-        print('Created object shape=',self.image.shape,'center=',self.center,'coords=',self.coords)
+        print('Created object shape=',self.image.shape,'center=',self.center,'coords=',self.coords,'area=',self.area)
