5da58e8b7dfea38591a0eb4e0673eddd9558e98c
joelmichelson <joelmichelson@gmail.com>
2018-10-08 14:22:53 -0500

    added segment_objects

--- SolidObject.py
+++ SolidObject.py
@@ -2,12 +2,13 @@ import numpy as np
 
 class SolidObject():
     
-    def __init__(self,array,coords):
+    def __init__(self,array,coords,pivot=None,isBlue=False):
         self.coords = coords
         self.image = array
         allpixels = np.transpose(array.nonzero())
         self.center = tuple(np.floor(np.mean(allpixels,axis=0)))
-        self.ropeIds = []
         self.area = sum(sum(array))
+        self.ropeIds = []
         self.ropeAttachPoints = []
+        self.pivot = pivot #make this coords of pivot
         print('Created object shape=',self.image.shape,'center=',self.center,'coords=',self.coords,'area=',self.area)
